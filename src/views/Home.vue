30a66007dab0ab34e6894af27ad4ac7d1d9c552a
Agung Priambada <thezeneight@gmail.com>
2021-10-19 15:22:18 +0800

    set state data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main v-if="!loading">
+    Show Data
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data...
+    </div>
+  </main>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: []
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary')
+      const data = await res.json()
+      return data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    
+    this.dataDate = data.dataDate
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
+  }
 }
 </script>
