88b9cc3fb1582d97c9e1b8021b11236dc94ba290
Agung Priambada <thezeneight@gmail.com>
2021-10-23 11:03:18 +0800

    add box and title component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
-  <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">
-      <i class="fa fa-viruses"></i>
-      Covid-19 Tracker</div>
-    <p>API by <a class="text-bblue-300" href="https://covid19api.com" target="_BLANK">covid19api.com</a>
-    </p>
-  </header>
+	<header class="text-center bg-blue-800 text-white p-4 mb-10">
+		<div class="text-3xl md:text-5xl font-bold mb-3">
+			<i class="fa fa-viruses"></i>
+			Covid-19 Tracker</div>
+		<p>API by <a class="text-bblue-300" href="https://covid19api.com" target="_BLANK">covid19api.com</a>
+		</p>
+	</header>
 </template>
 
 <script>
 export default {
-  name: 'Headder',
+	name: 'Header',
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,54 @@
 <template>
-  <main v-if="!loading">
-    Show Data
-  </main>
+	<main v-if="!loading">
+		<DataTitle :text="title" :dataDate="dataDate" />
 
-  <main class="flex flex-col align-center justify-center text-center" v-else>
-    <div class="text-gray-500 text-3xl mt-10 mb-6">
-      Fetching Data...
-    </div>
-  </main>
+		<DataBoxes :stats="stats" />
+
+		<CountrySelect :countries="countries" />
+	</main>
+
+	<main class="flex flex-col align-center justify-center text-center" v-else>
+		<div class="text-gray-500 text-3xl mt-10 mb-6">
+			Fetching Data...
+		</div>
+	</main>
 </template>
 
 <script>
+import DataTitle from "@/components/DataTitle"
+import DataBoxes from "@/components/DataBoxes"
+import CountrySelect from "@/components/CountrySelect";
+
 export default {
-  name: 'Home',
-  components: {},
-  data() {
-    return {
-      loading: true,
-      title: 'Global',
-      dataDate: '',
-      stats: {},
-      countries: []
-    }
-  },
-  methods: {
-    async fetchCovidData() {
-      const res = await fetch('https://api.covid19api.com/summary')
-      const data = await res.json()
-      return data
-    }
-  },
-  async created() {
-    const data = await this.fetchCovidData()
-    
-    this.dataDate = data.dataDate
-    this.stats = data.Global
-    this.countries = data.countries
-    this.loading = false
-  }
+	name: 'Home',
+	components: {
+		DataTitle,
+		DataBoxes,
+		CountrySelect
+	},
+	data() {
+		return {
+			loading: true,
+			title: 'Global',
+			dataDate: '',
+			stats: {},
+			countries: []
+		}
+	},
+	methods: {
+		async fetchCovidData() {
+			const res = await fetch('https://api.covid19api.com/summary')
+			const data = await res.json()
+			return data
+		}
+	},
+	async created() {
+		const data = await this.fetchCovidData()
+		
+		this.dataDate = data.Date
+		this.stats = data.Global
+		this.countries = data.Countries
+		this.loading = false
+	}
 }
 </script>
